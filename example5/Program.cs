2e3de966cef9b8bdd6744c5970dfc40af151fd69
Katsiaryna <katsiara@mail.ru>
2023-08-01 22:15:39 +0300

    For Masha

---
+++ example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Enter your name:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Yo! Masha is here!");
+}
+else
+{
+    Console.WriteLine("Hallo,");
+    Console.WriteLine(username);
+}
